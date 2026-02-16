e0ea723670d295f158ccf6b7c7cd624bb7ef6086
Tejas01-developes <tlimbad7@gmail.com>
2026-02-16 21:28:16 +0530

    learned create pool for database

--- Connections/Mysql.js
+++ Connections/Mysql.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-export const db=mysql.createConnection({
+export const db=mysql.createPool({
     port:process.env.SQL_PORT,
     host:process.env.SQL_HOST,
     user:process.env.SQL_ROOT,
